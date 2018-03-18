e9fef50ab74e54b2d0756fb0f50e3abc815fa174
sendai40 <ms40.kwh@gmail.com>
2018-03-18 12:37:47 +0900

    6th day

---
+++ 06_day/harib03c/bootpack.c
@@ -0,0 +1,26 @@
+// bootpackのメイン
+
+#include "bootpack.h"
+#include <stdio.h>
+
+void HariMain(void)
+{
+    struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+    char s[40], mcursor[256];
+    int mx, my;
+
+    init_gdtidt();
+    init_palette();
+    init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+    mx = (binfo->scrnx - 16) / 2;   // 画面中央になるように座標計算
+    my = (binfo->scrny - 28 - 16) / 2;
+    init_mouse_cursor8(mcursor, COL8_008484);
+    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+    sprintf(s, "scrnx = %d", binfo->scrnx);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
+
+
+    for (;;) {
+        io_hlt();
+    }
+}
